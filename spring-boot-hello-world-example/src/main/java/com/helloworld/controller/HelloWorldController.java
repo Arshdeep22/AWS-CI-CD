38eadcc312951fab42903cec60cf7adc71a1297c
Arshdeep Singh <arshdeep.singh04@sap.com>
2022-05-18 15:39:52 +0530

    update controller

--- spring-boot-hello-world-example/src/main/java/com/helloworld/controller/HelloWorldController.java
+++ spring-boot-hello-world-example/src/main/java/com/helloworld/controller/HelloWorldController.java
@@ -7,6 +7,6 @@ public class HelloWorldController
 @RequestMapping("/")
 public String hello() 
 {
-return "<h1> Congratulations. This is our now application. </h1>";
+return "<img src='https://images.contentstack.io/v3/assets/blt300387d93dabf50e/blt6aaaab200d5013ff/59131a0f59b80e7305b104bd/one-does-not-simply-implement-continuous-delivery.jpg?format=webp&width=1200' />";
 }
 }
