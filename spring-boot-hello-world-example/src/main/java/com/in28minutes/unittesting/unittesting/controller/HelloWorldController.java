61a5c358d1c3ecf84f3f3e679353f47d5c26f14c
Arshdeep Singh <arshdeep.singh04@sap.com>
2022-05-20 13:00:20 +0530

    Ui update

--- spring-boot-hello-world-example/src/main/java/com/in28minutes/unittesting/unittesting/controller/HelloWorldController.java
+++ spring-boot-hello-world-example/src/main/java/com/in28minutes/unittesting/unittesting/controller/HelloWorldController.java
@@ -8,7 +8,7 @@ public class HelloWorldController {
 
 	@GetMapping("/hello-world")
 	public String helloWorld() {
-		return "Hello World";
+		return "<img src='https://images.contentstack.io/v3/assets/blt300387d93dabf50e/blt6aaaab200d5013ff/59131a0f59b80e7305b104bd/one-does-not-simply-implement-continuous-delivery.jpg?format=webp&width=1200' />";
 	}
 	
 }
--- spring-boot-hello-world-example/src/test/java/com/in28minutes/unittesting/unittesting/controller/HelloWorldControllerTest.java
+++ spring-boot-hello-world-example/src/test/java/com/in28minutes/unittesting/unittesting/controller/HelloWorldControllerTest.java
@@ -28,11 +28,8 @@ public class HelloWorldControllerTest {
 		
 		MvcResult result = mockMvc.perform(request)
 				.andExpect(status().isOk())
-				.andExpect(content().string("Hello World"))
+				.andExpect(content().string("<img src='https://images.contentstack.io/v3/assets/blt300387d93dabf50e/blt6aaaab200d5013ff/59131a0f59b80e7305b104bd/one-does-not-simply-implement-continuous-delivery.jpg?format=webp&width=1200' />"))
 				.andReturn();
-	
-		//verify "Hello World"
-		//assertEquals("Hello World", result.getResponse().getContentAsString());
 	}
 
 }
